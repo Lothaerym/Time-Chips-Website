eddb90c8a24ea4df11b56affb6296a5d233f935a
DELHAYE Thomas <thomas.del.haye@outlook.fr>
2020-06-20 15:12:32 +0200

    Menu entity and form + drop down articles

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,21 +2,83 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Menu;
+use App\Form\MenuType;
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/menus")
+ */
 class MenuController extends AbstractController
 {
-	/**
-     * @Route("/menus")
+    /**
+     * @Route("/", name="menu_index", methods={"GET"})
      */
-    public function menus()
+    public function index(MenuRepository $menuRepository): Response
     {
-
-    	$test = "test";
-
         return $this->render('listMenus.html.twig', [
-            'test' => $test,
+            'menus' => $menuRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="menu_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $menu = new Menu();
+        $form = $this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($menu);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('menu_index');
+        }
+
+        return $this->render('menu/new.html.twig', [
+            'menu' => $menu,
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    /**
+     * @Route("/{id}/edit", name="menu_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Menu $menu): Response
+    {
+        $form = $this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('menu_index');
+        }
+
+        return $this->render('menu/edit.html.twig', [
+            'menu' => $menu,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="menu_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Menu $menu): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$menu->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($menu);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('menu_index');
     }
 }
