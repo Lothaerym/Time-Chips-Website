2a13df4da5f666ac11de55d10290252b41255831
DELHAYE Thomas <thomas.del.haye@outlook.fr>
2020-06-28 21:49:28 +0200

    Album

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
+use App\Form\ImageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AlbumController extends AbstractController
 {
@@ -17,8 +23,53 @@ class AlbumController extends AbstractController
      */
     public function index()
     {
+        // Files list (photos)
+        $scandir = scandir("./../public/uploads/photo");
+
         return $this->render('album/index.html.twig', [
-            'controller_name' => 'AlbumController',
+            'photos' => $scandir,
         ]);
     }
+    
+    
+    /**
+     * @Route("/image/new", name="image_new")
+     */
+    public function new(Request $request, SluggerInterface $slugger)
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $photoFile = $form->get('photo')->getData();
+
+
+            if ($photoFile) {
+                $originalFilename = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photoFile->guessExtension();
+
+                try {
+                    $photoFile->move(
+                        $this->getParameter('photo_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    echo("Probleme");
+                }
+
+                $image->setName($newFilename);
+            }
+
+
+            return $this->redirect($this->generateUrl('album'));
+        }
+
+            return $this->render('image/newImage.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
 }
---
+++ src/Form/ImageType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('photo', FileType::class, [
+                'label' => 'Photo',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Fichier invalide',
+                    ])
+                ],
+            ])
+            // ...
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
